fff7a9d64eaea6e908eed9af2709b8c5f1853aef
Toluchuru Haritha <harit@haritha@gmail.com>
2025-03-18 10:11:36 +0530

    save all

--- app.py
+++ app.py
@@ -6,33 +6,33 @@ todo=Flask('__name__')
 students=[
         {
             'std_id':1,
-            'student_name':'Haritha',
-            'age':20,
-            'email':'harithat2004@gmail.com'
+            'student_name':'ramya',
+            'age':21,
+            'email':'ramyapramya178@gmail.com'
         },
         {
             'std_id':2,
-            'student_name': 'kavya',
+            'student_name': 'sharath',
             'age': 20,
-            'email': 'kavyanaidu359@gmail.com'
+            'email': 'sharath334@gmail.com'
         },
         {
             'std_id':3,
-            'student_name': 'ramya',
+            'student_name': 'deeskitha',
             'age': 21,
-            'email': 'ramyapramya@gmail.com'
+            'email': 'deekitha2004@gmail.com'
         },
         {
             'std_id':4,
-            'student_name': 'abhishek',
+            'student_name': 'sami',
             'age': 20,
-            'email': 'a17182649gmail.com'
+            'email': 'sami@gmail.com'
         },
         {
             'std_id':5,
-            'student_name': 'harshitha',
+            'student_name': 'aslam',
             'age': 21,
-            'email': 'toluchuruharshitha@gmail.com'
+            'email': 'aslam262004@gmail.com'
         },
 
     ]
@@ -41,6 +41,19 @@ students=[
 def student_list():
     return jsonify(students)
 
+@todo.route('/students-list/restapi')
+def student_list_restapi():
+    import requests
+
+    url = "https://restapi-test-one-2-gum5.onrender.com/students-list"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url)
+
+    return response.json()
+
 @todo.route('/student/get/<int:std_id>')
 def student_get_by_id(std_id):
     for std in students:
@@ -48,11 +61,9 @@ def student_get_by_id(std_id):
             return jsonify(std)
     return "id not found"
 
-
-
-
-
 if __name__ == '__main__':
     todo.run(
+        host='127.0.0.1',
+        port=5010,
         debug=True
     )
